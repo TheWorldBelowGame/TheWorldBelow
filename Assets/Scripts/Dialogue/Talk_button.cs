4370d7ea44103140fe4b58534326f094bd5a0bbe
Nicole Cischke <cischken@umich.edu>
2016-03-14 01:12:16 -0400

    Things are getting prettier

---
+++ Assets/Scripts/Dialogue/Talk_button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Talk_button : MonoBehaviour {
+
+	public Sprite button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Color tmp = GetComponent<SpriteRenderer> ().color;
+
+
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("inside");
+			tmp.a = 1f;
+			GetComponent<SpriteRenderer> ().color = tmp;
+		} else {
+			tmp.a = 0f;
+			GetComponent<SpriteRenderer> ().color = tmp;
+		}
+	}
+}
