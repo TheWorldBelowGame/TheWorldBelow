88462f13709359a1899507f9d261f6d46c565828
Jake Haas <haasjac@gmail.com>
2016-03-17 00:57:25 -0400

    Buttons work

--- Assets/Scripts/Game/Global.cs
+++ Assets/Scripts/Game/Global.cs
@@ -19,4 +19,5 @@ public class Global {
         killed = 0;
         collected = 0;
     }
+    
 }
---
+++ Assets/Scripts/Game/Levels.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour {
+
+    public void gotoLevel(string sname) {
+        SceneManager.LoadScene(sname);
+    }
+}
