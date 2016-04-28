3b20abf5ec83a60094756105e5c9dd71105c8cdd
Tomas <tomasmed@umich.edu>
2016-04-27 23:36:42 -0400

    First Light+Traveling Mechanic: The platform discovery projectile

---
+++ Assets/Scripts/Hidden_Platform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hidden_Platform : MonoBehaviour {
+
+    private SpriteRenderer sprend;
+    void Start()
+    {
+        sprend = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void togglePlatform()
+    {
+        if (sprend.enabled) sprend.enabled = false;
+        else sprend.enabled = true;
+    }
+}
