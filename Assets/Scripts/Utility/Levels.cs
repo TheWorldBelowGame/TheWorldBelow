34aebb67fd6a1e52ff42ca6fbf2c68d2d2bc6589
Cooper Riehl <Cooper Riehl>
2016-05-18 09:57:29 -0400

    Moved Levels script to Utility folder and updated scenes with buttons in them

--- Assets/Scripts/Utility/Levels.cs
+++ Assets/Scripts/Utility/Levels.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 // Utility class for changing levels.
-public class Levels
+public class Levels : MonoBehaviour
 {
     public void GotoLevel(string sceneName)
 	{
